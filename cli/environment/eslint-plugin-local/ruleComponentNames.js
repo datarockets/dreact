92d75cf3610c8560d2593ed82a8f95a5363a6618
Eugene Zhlobo <ezhlobo@gmail.com>
2020-01-02 15:34:06 +0300

    Make sure we require correct names for pages

--- cli/environment/eslint-plugin-local/ruleComponentNames.js
+++ cli/environment/eslint-plugin-local/ruleComponentNames.js
@@ -27,7 +27,7 @@ function getExpectedName(filepath) {
 
   if (isPageComponent.match(filepath)) {
     return `Page${filepath
-      .replace(/^.+\/src\/pages\/([\w.]+)(\/index\.js|\.js)/, '$1')
+      .replace(/^.+\/src\/pages((?:\/[A-Z][A-z]*)+).+$/, '$1')
       .replace(/\//g, '')}`
   }
 
