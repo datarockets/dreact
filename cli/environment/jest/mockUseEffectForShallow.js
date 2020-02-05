29accca3738be2376cf5d66bb7363b5f8fa7821d
Eugene Zhlobo <ezhlobo@gmail.com>
2020-02-05 21:10:49 +0300

    Execute useEffect in tests when the local state changes

--- cli/environment/jest/mockUseEffectForShallow.js
+++ cli/environment/jest/mockUseEffectForShallow.js
@@ -9,17 +9,34 @@ jest.mock('react', () => ({
       : jest.requireActual('react').useEffect(cllb, deps),
 }))
 
+const mockWithUseEffect = callback => {
+  mockUseEffects = []
+  mockBoolWhenNeedMockHook = true
+
+  const result = callback()
+
+  mockUseEffects.forEach(cllb => cllb())
+  mockBoolWhenNeedMockHook = false
+
+  return result
+}
+
 jest.mock('dreact/helper-test', () => {
   const unmocked = jest.requireActual('dreact/helper-test')
 
-  const shallow = (...args) => {
-    mockUseEffects = []
-    mockBoolWhenNeedMockHook = true
+  // We mock ShallowWrapper to make sure we execute all useEffects after
+  class ShallowWrapper extends unmocked.ShallowWrapper {
+    constructor(...args) {
+      mockWithUseEffect(() => super(...args))
+    }
+  }
 
-    const result = unmocked.shallow(...args)
+  const shallow = (node, options) => {
+    const result = new ShallowWrapper(node, null, options)
 
-    mockUseEffects.forEach(cllb => cllb())
-    mockBoolWhenNeedMockHook = false
+    // `rerender` method triggered internally by `setProps`
+    const rerender = result.rerender.bind(result)
+    result.rerender = (...args) => mockWithUseEffect(() => rerender(...args))
 
     return result
   }
