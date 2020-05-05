c2663c305d0ad7e5e865b1482887bfe3348d9c9a
kelvin0722 <kelvinm442@gmail.com>
2020-05-05 14:59:17 +0300

    Allow imports from src root directory

--- cli/environment/eslint.config.js
+++ cli/environment/eslint.config.js
@@ -80,6 +80,7 @@ const internalConfig = {
         groups: [
           '/^(assert|async_hooks|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|http2|https|inspector|module|net|os|path|perf_hooks|process|punycode|querystring|readline|repl|stream|string_decoder|timers|tls|trace_events|tty|url|util|v8|vm|zli)/',
           '/^((?!src|UI|Icons)\\w[\\w-\\./]+|@.+)$/',
+          '^/src',
           '/^src\\/pages\\/.+$/',
           '/^src\\/(?!services|collections|containers|components|forms|lib)[\\w-/]+$/',
           '/^src\\/services(\\/.+)?/',
